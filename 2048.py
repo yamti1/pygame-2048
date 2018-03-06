c7484c3819630355e8474a8611562732c8ff844b
unknown <yam.tirosh@gmail.com>
2018-03-06 16:31:29 +0200

    write spawn_tile()

--- 2048.py
+++ 2048.py
@@ -1,7 +1,9 @@
 from pyprocessing import *
 import numpy as np
+import random
 
 BOARD_SIZE = 4, 4  # num_rows, num_columns
+PROB_SPAWN_2 = .75  # the probability to spawn a '2' tile instead of a '4' tile
 
 board = np.zeros(BOARD_SIZE, int)
 
@@ -16,6 +18,32 @@ def merge_tiles(tiles):
         i += 1
 
     return tiles  # for convenience. the tiles are merged in-place.
+
+
+def spawn_tile():
+    """
+    Spawns a new tile in a clear spot on the board.
+    The tile can be a '2' tile or a '4' tile, randomly according to PROB_SPAWN_2.
+    :return: The number of clear spots on the board after spawning.
+    """
+    clear_tiles = []
+    for i, row in enumerate(board):
+        for j, tile in enumerate(row):
+            if tile == 0:
+                clear_tiles.append((i, j))
+
+    if clear_tiles:
+        board[random.choice(clear_tiles)] = 2 if random.random() < PROB_SPAWN_2 else 4
+
+    return len(clear_tiles) - 1
+
+
+def is_game_over():
+    pass
+
+
+def game_over():
+    pass
 
 
 def keyPressed():
@@ -46,6 +74,10 @@ def keyPressed():
             reversed_row += [0] * (BOARD_SIZE[1] - len(reversed_row))
             board[i, :] = reversed(reversed_row)
 
+    spawn_tile()
+
+    if is_game_over():
+        game_over()
 
 
 def setup():
